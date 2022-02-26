23e526169e53a54c7bfedd9f36246b29383fdee2
irfangeliga <muhammad.2960@students.amikom.ac.id>
2022-02-26 17:26:00 +0700

    first commit

---
+++ web.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+class Fruit {
+    public $name;
+    public $color;
+
+    function __construct($name, $color){
+        $this ->name = $name;
+        $this ->color = $color;
+    }
+
+    function __destruct(){
+        echo "buah {$this->name} memiliki warna {$this->color}.<br/>";
+    }
+}
+ $apple = new Fruit("Kuning","Nangka");
+ echo $apple->name." adalah buah asal indonesia <br/>";
+ echo "UJIAN UTS PWL 2021<br/>";
+ 
+?>
+</body>
+</html>
